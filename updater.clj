2f42ac4fc1ff2c121b7833f3882dadb323d5e385
timsgardner <timsgardner@gmail.com>
2015-05-04 19:54:25 -0400

    fixing contents of updater.clj

---
+++ updater.clj
@@ -0,0 +1,32 @@
+(ns clojure-west.updater
+  (:use arcadia.core)
+  (:require [arcadia.internal.map-utils :as mu]
+            arcadia.inspectors))
+
+(def disabled
+  (atom #{}))
+
+(def updater-fns
+  (atom {}))
+
+(defn put [k updr]
+  (swap! updater-fns assoc k updr))
+
+(defn disable [k]
+  (swap! disabled conj k))
+
+(defn enable [k]
+  (swap! disabled disj k))
+
+(defn toggle [k]
+  (swap! disabled #(if (# k) (disj # k) (conj # k))))
+
+(defn run-updaters [updater]
+  (let [ds @disabled]
+   (doseq [[k f] @updater-fns
+           :when (not (ds k))]
+     (f))))
+
+(defcomponent Updater []
+  (Update [this]
+    (run-updaters @updaters)))
