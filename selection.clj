3c804bbad0d03e07c652cdfce55c1084fa4c3a68
timsgardner <timsgardner@gmail.com>
2015-07-14 16:50:09 -0400

    improving selection.clj

--- selection.clj
+++ selection.clj
@@ -1,18 +1,58 @@
 (ns clojure-west.selection
   (:use arcadia.core)
-  (:require [arcadia.introspection :as it]))
+  (:require [clojure-west.raycast :as ry]))
+
+;; ============================================================
+;; basics
+;; ============================================================
 
 (def global-selection
   (atom #{}))
 
+(def ^:dynamic *selection*
+  global-selection)
+
 (defn sel
-  ([] @global-selection)
-  ([x] (reset! global-selection #{x}))
+  ([] @*selection*)
+  ([x] (reset! *selection* #{x}))
   ([x & xs]
-   (reset! global-selection (into #{} (conj x xs)))))
+   (reset! *selection* (into #{x} xs))))
+
+(defn sel+
+  ([] @*selection*)
+  ([x] (swap! *selection* conj x))
+  ([x & xs]
+   (swap! *selection* into (cons x xs))))
+
+(defn sel-
+  ([] @*selection*)
+  ([x] (swap! *selection* disj x))
+  ([x & xs]
+   (swap! *selection*
+     #(reduce disj % (cons x xs)))))
+
+(defn selc [coll]
+  (reset! *selection*
+    (set coll)))
 
 (defn fsel []
-  (first @global-selection))
+  (first @*selection*))
 
 (defn selall [coll]
-  (reset! global-selection (into #{} coll)))
+  (reset! *selection* (into #{} coll)))
+
+(defn desel []
+  (reset! *selection* #{}))
+
+;; ============================================================
+;; finesse
+;; ============================================================
+
+(defn forward [x]
+  (sel (ry/raycast-forward x)))
+
+(defn forward+ [x]
+  (sel+ (ry/raycast-forward x)))
+
+(defn forward- [x]
+  (sel- (ry/raycast-forward x)))
